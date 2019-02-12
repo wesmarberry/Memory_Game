2d25ee3dd12cfc17be9030e0b8124e42a580e384
Wes Marberry <wesmarberry@gmail.com>
2019-02-11 20:39:38 -0600

    created game logic

--- memory_game/javascript/scripts.js
+++ memory_game/javascript/scripts.js
@@ -13,17 +13,29 @@
 
 var cards = ["queen", "queen", "king","king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2)	{
+//var cardOne = cards[0];
+//cardsInPlay.push(cardOne);
+//console.log("User flipped " + cardOne);
+//var cardTwo = cards[2];
+//cardsInPlay.push(cardTwo);
+//console.log("User flipped " + cardTwo);
+var checkForMatch = function ()	{
 	if(cardsInPlay[0] === cardsInPlay[1])	{
 		alert("You Found A Match!");
-	}
-	else {
+	} else {
 		alert("Sorry, Try Again.");
 	}
-}
+};
+
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2)	{
+		checkForMatch();
+	};
+};
+
+
+
+flipCard(0);
+flipCard(2);

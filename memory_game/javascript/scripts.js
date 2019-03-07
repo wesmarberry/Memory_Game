e55d796f13af578282a976bad5c76344bee21ffb
Wes Marberry <wesmarberry@gmail.com>
2019-03-06 20:16:48 -0600

    created game logic

--- memory_game/javascript/scripts.js
+++ memory_game/javascript/scripts.js
@@ -15,22 +15,26 @@ var cards = [
 {
 rank: "queen",
 suit: "hearts",
-cardImage: "images/queen-of-hearts.png" 	
+cardImage: "images/queen-of-hearts.png",
+cardBack: "images/back.png"	
 },
 {
 rank: "queen",
 suit: "diamonds",
-cardImage: "images/queen-of-diamonds.png" 	
+cardImage: "images/queen-of-diamonds.png",
+cardBack: "images/back.png"	
 },
 {
 rank: "king",
 suit: "hearts",
-cardImage: "images/king-of-hearts.png" 	
+cardImage: "images/king-of-hearts.png",
+cardBack: "images/back.png"		
 },
 {
 rank: "king",
 suit: "diamonds",
-cardImage: "images/king-of-diamonds.png" 	
+cardImage: "images/king-of-diamonds.png",
+cardBack: "images/back.png"		
 }
 ];
 var cardsInPlay = [];
@@ -40,25 +44,72 @@ var cardsInPlay = [];
 //var cardTwo = cards[2];
 //cardsInPlay.push(cardTwo);
 //console.log("User flipped " + cardTwo);
+var scoreCount = 0;
 var checkForMatch = function ()	{
 	if(cardsInPlay[0] === cardsInPlay[1])	{
 		alert("You Found A Match!");
+		scoreCount = scoreCount + 1;
+		var oldSpan = document.createElement("span");
+		oldSpan.id = "childSpan";
+		var oldSpan_content = document.createTextNode(scoreCount);
+		oldSpan.appendChild(oldSpan_content);
+		var newSpan = document.getElementById("childSpan");
+		var parentDiv = newSpan.parentNode;
+		parentDiv.replaceChild(oldSpan, newSpan);
 	} else {
 		alert("Sorry, Try Again.");
 	}
 };
 
-var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+	//console.log("User flipped " + cards[cardId].rank);
+	//console.log(cards[cardId].cardImage);
+	//console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2)	{
 		checkForMatch();
 	};
 };
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 
-flipCard(0);
-flipCard(2);
+
+
+
+var createBoard = function ()	{
+	for (var i = 0; i < cards.length; i++)	{
+		var randomArray = [0, 1, 2, 3];
+		randomArray = shuffle(randomArray);
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id',randomArray[i]);
+		cardElement.setAttribute('id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	};
+};
+
+var resetBoard = function ()	{
+		var removeCards = document.getElementById('game-board');
+		removeCards.innerHTML = '';
+		createBoard();
+		cardsInPlay = [];
+};
+
+createBoard();
+document.querySelector('.reset').addEventListener('click', resetBoard)
